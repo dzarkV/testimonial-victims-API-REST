2f538e384a5d7bc874387a38c16cd5cede7d9d47
dzarkV <dfvargasp@unal.edu.co>
2022-07-12 19:39:45 -0500

    Check validation GET id

--- docs.py
+++ docs.py
@@ -1,4 +1,4 @@
 tags_metadata = [{
     "name": "testimonios",
-    "description": "42 testimonios del libro de las anticipaciones con todos sus atributos"
+    "description": "Testimonios del Libro de las anticipaciones con todos sus atributos"
 }]
--- routes/testimonio.py
+++ routes/testimonio.py
@@ -5,11 +5,11 @@ from schemas.testimonio import testimonioEntity, testimoniosEntity
 
 testimonio = APIRouter()
 
-@testimonio.get('/testimonios', response_model=list[Testimonio], tags=['testimonios'])
+@testimonio.get('/testimonios', response_model=list[Testimonio], tags=['testimonios'], description="Obtiene los 42 testimonios del Libro de las anticipaciones.")
 async def find_all_testimonio()->list:
     return testimoniosEntity(collection.find())
 
-@testimonio.get('/testimonios/{id}', response_model=Testimonio, tags=['testimonios'])
+@testimonio.get('/testimonios/{id}', response_model=Testimonio, tags=['testimonios'], description="Obtiene el testimonio según su identificador. Actualmente solo hay 42 testimonios.")
 async def find_testimonio(id: int)->dict:
     if id > collection.estimated_document_count():
         raise HTTPException(status_code=404, detail = f"No hay testimonio con el id {id}.")
