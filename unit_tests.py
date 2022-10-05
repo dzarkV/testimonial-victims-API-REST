6bd2d5e68e46c3ef474506226970e147d711753e
dzarkV <dfvargasp@unal.edu.co>
2022-10-04 20:12:14 -0500

    new unit test

--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,5 @@
 from schemas.lugar import lugaresEntity
+from schemas.persona import personasEntity
 from routes.search import search_by_text
 
 
@@ -27,23 +28,23 @@ class TestClass():
                 ])
         assert (resultTest == testOkLugares)
 
-    async def test_routes_search(self):
-        testOkSearch = {"hits": [
-                          {"id": 32,
-                            "titulo": "Tomar la delantera",
-                            "texto": "Ya hacía tres años que había construido mi rancho allá... Por llegar más rápido, yo siempre me iba por un camino de atajo que dicen; y ahí estaba la bomba. Cuando nosotros llegamos ahí no había ni el perro, solo polvo. \nY ya, no nos siguió más el perrito. \n\n"
-                          }
-                        ],
-                        "estimatedTotalHits": 1,
-                        "query": "ELN",
-                        "limit": 20,
-                        "offset": 0,
-                        "processingTimeMs": 81
-                        }
-        resultTest = await search_by_text(q="ELN")
-        assert (resultTest == testOkSearch)
+    def test_schema_personas(self):
+        testOkPersonas = [{
+                "id": 1,
+                "personas": ["Lucas", "Margarita", "William"]},
+                {
+                "id": 2,
+                "personas": ["Daniel", "María Sanabria"]}]
+        resultTest = personasEntity([
+                {"id": 1,
+                "value":{
+                "persona": ["Lucas", "Margarita", "William"]}},
+                {"id": 2,
+                "value":{
+                "persona": ["Daniel", "María Sanabria"]}}])
+        assert (resultTest == testOkPersonas)
 
-    async def test_routes_search2(self):
+    async def test_routes_search(self):
         testOkSearch2 = {"detail": {
                             "message": "word not found in testimonies",
                             "query": "00000000"}
